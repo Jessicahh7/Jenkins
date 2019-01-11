6c035d3390dc65305b745d29412beb8a10b37181
User <user@Users-MacBook-Pro.local>
2019-01-11 11:00:05 -0500

    add semicolon

--- Testing.java
+++ Testing.java
@@ -3,7 +3,8 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Jenkins")
+		System.out.println("Hello Jenkins");
 	}
 
 }
+
